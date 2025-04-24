314dbb97ceb512456a18c853d0fd7c871dea5259
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2025-04-24 17:17:48 -0400

    Better documentation for allocator

--- lib/src/mem/allocator.h
+++ lib/src/mem/allocator.h
@@ -28,8 +28,10 @@ typedef struct SyAllocator {
 extern "C" {
 #endif
 
+/// @returns `NULL` if memory allocation fails for whatever reason.
 SY_API void* sy_allocator_alloc(SyAllocator* self, size_t len, size_t align);
 
+/// @param `buf` Non-null. 
 SY_API void sy_allocator_free(SyAllocator* self, void* buf, size_t len, size_t align);
 
 SY_API void sy_allocator_destructor(SyAllocator* self);
