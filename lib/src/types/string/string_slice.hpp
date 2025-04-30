0534cbed3527d3365c633de8b3806c3a6aac0d78
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2025-04-30 14:54:23 -0400

    Create string_slice.hpp

---
+++ lib/src/types/string/string_slice.hpp
@@ -0,0 +1,33 @@
+//! API
+#pragma once
+#ifndef SY_TYPES_STRING_STRING_SLICE_HPP_
+#define SY_TYPES_STRING_STRING_SLICE_HPP_
+
+
+namespace sy {
+    namespace c {
+        #include "string_slice.h"
+    }
+
+    class StringSlice {
+    public:
+        StringSlice() = default;
+
+        StringSlice(const StringSlice&) = default;
+        StringSlice& operator=(const StringSlice&) = default;
+        StringSlice(StringSlice&&) = default;
+        StringSlice& operator=(StringSlice&&) = default;
+
+        template<size_t N>
+        StringSlice(char const (&inStr)[N]) : _inner{inStr, N - 1} {}
+
+        [[nodiscard]] const char* data() const { return _inner.ptr; }
+
+        [[nodiscard]] size_t len() const { return _inner.len; }
+
+    private:
+        c::SyStringSlice _inner = {0, 0};
+    };
+}
+
+#endif // SY_TYPES_STRING_STRING_SLICE_HPP_
