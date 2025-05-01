9dcd7cb2df10bc6677f3ea54a349b75256e3737b
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2025-05-01 17:18:03 -0400

    scalar primitive types

--- lib/src/interpreter/bytecode.cpp
+++ lib/src/interpreter/bytecode.cpp
@@ -1,7 +1,29 @@
 #include "bytecode.hpp"
+#include "../types/type_info.hpp"
+#include "../util/unreachable.hpp"
 
 OpCode Bytecode::getOpcode() const
 {
     // This is safe.
     return static_cast<OpCode>(this->value & OPCODE_BITMASK);
+}
+
+const sy::Type *scalarTypeFromTag(ScalarTag tag)
+{
+    switch(tag) {
+        case ScalarTag::Bool: return sy::Type::TYPE_BOOL;
+        case ScalarTag::I8: return sy::Type::TYPE_I8;
+        case ScalarTag::I16: return sy::Type::TYPE_I16;
+        case ScalarTag::I32: return sy::Type::TYPE_I32;
+        case ScalarTag::I64: return sy::Type::TYPE_I64;
+        case ScalarTag::U8: return sy::Type::TYPE_U8;
+        case ScalarTag::U16: return sy::Type::TYPE_U16;
+        case ScalarTag::U32: return sy::Type::TYPE_U32;
+        case ScalarTag::U64: return sy::Type::TYPE_U64;
+        case ScalarTag::USize: return sy::Type::TYPE_USIZE;
+        case ScalarTag::F32: return sy::Type::TYPE_F32;
+        case ScalarTag::F64: return sy::Type::TYPE_F64;
+    }
+    unreachable();
+    return nullptr;
 }
--- lib/src/interpreter/bytecode.hpp
+++ lib/src/interpreter/bytecode.hpp
@@ -4,6 +4,10 @@
 
 #include "../core.h"
 
+namespace sy {
+    struct Type;
+}
+
 enum class OpCode : uint8_t {
     Nop = 0,
     LoadZero,
@@ -34,11 +38,30 @@ enum class OpCode : uint8_t {
 constexpr size_t OPCODE_USED_BITS = 8;
 constexpr size_t OPCODE_BITMASK = 0b11111111;
 
+/// Zero initializing gets a Nop.
 struct Bytecode {
     uint64_t value;
 
     OpCode getOpcode() const;
 };
 
+enum class ScalarTag : uint8_t {
+    Bool,
+    I8,
+    I16,
+    I32,
+    I64,
+    U8,
+    U16,
+    U32,
+    U64,
+    USize,
+    F32,
+    F64,
+};
+
+constexpr size_t SCALAR_TAG_USED_BITS = 6;
+
+const sy::Type* scalarTypeFromTag(ScalarTag tag);
 
 #endif // SY_INTERPRETER_BYTECODE_HPP_
