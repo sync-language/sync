9cad8e1b5955831f7787bebe70540073eb459973
Gabriel Khan-Figueroa <gkhan-figueroa@myseneca.ca>
2025-05-23 14:26:36 -0400

    more extra

--- lib/src/types/string/string.hpp
+++ lib/src/types/string/string.hpp
@@ -11,6 +11,7 @@ namespace sy {
     /// Dynamic, [Small String Optimized](https://giodicanio.com/2023/04/26/cpp-small-string-optimization/) 
     /// string class. It's a primitive script type.
     class SY_API String final {
+    public:
 
         // Default constructor
 
@@ -28,9 +29,26 @@ namespace sy {
 
         // const char* constructor
 
+        // Get length
+
         // Get as string slice
 
         // Get as const char*
+
+
+    private:
+
+        // allocate buffer
+
+        // free buffer
+
+        // check if using sso
+
+        // set sso flag
+
+    private:
+
+        // members
     };
 }
 
