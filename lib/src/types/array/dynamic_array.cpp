804a0accd5087f78c31f5e1f704a180e47fa1f05
Dillon Kwak <dkwak3+seneca@myseneca.ca>
2025-05-27 18:39:08 -0400

    initial work on DynArray and 2 tests:

--- lib/src/types/array/dynamic_array.cpp
+++ lib/src/types/array/dynamic_array.cpp
@@ -2,3 +2,40 @@
 #include "dynamic_array.hpp"
 #include "../../mem/allocator.hpp"
 #include "../../util/assert.hpp"
+
+#if SYNC_LIB_TEST
+
+#include "../../doctest.h"
+
+using sy::DynArray;
+
+TEST_CASE("default construction") {
+    DynArray<size_t> arr;
+    CHECK_EQ(arr.len(), 0);
+}
+
+TEST_SUITE("push const T&") {
+    TEST_CASE("push 1") {
+        DynArray<size_t> arr;
+        const size_t element = 2;
+        arr.push(element); 
+
+        CHECK_EQ(arr.len(), 1);
+        CHECK_EQ(arr[0], element);
+    }
+
+
+    TEST_CASE("push 1") {
+        DynArray<size_t> arr;
+        const size_t element1 = 5;
+        const size_t element2 = 10;
+        arr.push(element1); 
+        arr.push(element2); 
+
+        CHECK_EQ(arr.len(), 2);
+        CHECK_EQ(arr[0], element1);
+        CHECK_EQ(arr[1], element2);
+    }
+}
+
+#endif
--- lib/src/types/array/dynamic_array.hpp
+++ lib/src/types/array/dynamic_array.hpp
@@ -7,9 +7,63 @@
 namespace sy {
     
     /// Dynamically resizable array. Grows to fit elements you push into it.
+    template <typename T>
     class DynArray {
+        T*      _data =nullptr;
+        size_t  _length =0;
+        size_t  _capacity =0;
+    public:
+        DynArray()=default;
+        ~DynArray(){
+            if(_data == nullptr){
+                return;
+            }
+            for(size_t i=0; i<_length; i++){
+                _data[i].~T();
+            }
+            delete[] _data;
+            _data = nullptr;
+        };
+        void push(const T& v){
+            if(_length == _capacity){
+                const size_t newCapacity = calculateNewCapacity(_capacity);
+                T* temp = new T[newCapacity];
+                for(size_t i = 0; i<_length; i++){
+                    temp[i] = _data[i];
+                }
+                delete[] _data;
+                _data = temp;
+                _capacity = newCapacity;
+            }
+            //just put the element to the end, set the data as new allocation
+            _data[_length] = v;
+            _length++;
+        }
+        void push(T&& v){
+            
+        };
+        const T& operator[](const size_t i) const{
+            //return data indiv
+            return _data[i];
+        };
+        T& operator[](const size_t i){
+            return _data[i];
+        };
 
+        size_t len() const { return _length; }
+
+    private:
+
+        static size_t calculateNewCapacity(size_t currentCapacity) {
+            if(currentCapacity == 0){
+                currentCapacity = 1;
+            }
+            currentCapacity *= 2;
+            return currentCapacity;
+        }
     };
-}
+    
+};
+
 
 #endif // SY_TYPES_ARRAY_DYNAMIC_ARRAY_HPP_
