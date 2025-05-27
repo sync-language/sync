31d0c71a875afed05187595bec8647efd17e5554
Gabriel Khan-Figueroa <gkhan-figueroa@myseneca.ca>
2025-05-27 12:10:41 -0400

    instruction

--- lib/src/types/array/dynamic_array.hpp
+++ lib/src/types/array/dynamic_array.hpp
@@ -4,4 +4,12 @@
 
 #include "../../core.h"
 
+namespace sy {
+    
+    /// Dynamically resizable array. Grows to fit elements you push into it.
+    class DynArray {
+
+    };
+}
+
 #endif // SY_TYPES_ARRAY_DYNAMIC_ARRAY_HPP_
