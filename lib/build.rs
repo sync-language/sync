de7a4507eb47ef134ac47c5c502ed71923edef2f
Gabriel Khan-Figueroa <gkhan-figueroa@myseneca.ca>
2025-06-22 12:16:10 -0400

    remove cmake crate

--- lib/build.rs
+++ lib/build.rs
@@ -1,20 +1,7 @@
-extern crate cmake;
 extern crate cc;
 
 fn main()
 {
-    // let dst = cmake::Config::new(".")
-    //     .no_build_target(true)
-    //     .always_configure(true)
-    //     .build();
-
-    // if cfg!(target_os = "macos") {
-    //     println!("cargo:rustc-flags=-l dylib=c++");    
-    // }   
-      
-    // println!("cargo:rustc-link-search=native={}", dst.display());
-    // println!("cargo:rustc-link-lib=static=SyncLib");
-    
     cc::Build::new()
         .cpp(true)
         .std("c++17")
