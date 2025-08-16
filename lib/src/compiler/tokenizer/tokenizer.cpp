6363962daa72c43ab84b192e22931efdc9e0ad2b
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2025-08-16 15:16:20 -0400

    work on tokenizer stuff

--- lib/src/compiler/tokenizer/tokenizer.cpp
+++ lib/src/compiler/tokenizer/tokenizer.cpp
@@ -11,7 +11,9 @@ Tokenizer::~Tokenizer()
     if(len_ == 0) return;
 
     Token* tokens = const_cast<Token*>(this->tokens_);
+    uint32_t* ends = const_cast<uint32_t*>(this->ends_);
     alloc_.freeArray(tokens, this->len_);
+    alloc_.freeArray(ends, this->len_);
 
     this->tokens_ = nullptr;
     this->len_ = 0;
@@ -42,6 +44,7 @@ Tokenizer &Tokenizer::operator=(Tokenizer &&other)
 std::variant<Tokenizer, CompileError> Tokenizer::create(Allocator allocator, StringSlice source)
 {
     Tokenizer self(allocator);
+    self.source_ = source;
 
     if(source.len() > MAX_SOURCE_LEN) {
         CompileError::FileTooBig fileTooBig;
@@ -54,11 +57,17 @@ std::variant<Tokenizer, CompileError> Tokenizer::create(Allocator allocator, Str
     // to the number of bytes in the source
     // As a result, we can over-allocate upfront.
     const size_t capacity = source.len();
-    auto bigMemRes = self.alloc_.allocAlignedArray<Token>(capacity, ALLOC_CACHE_ALIGN);
-    if(bigMemRes.hasValue() == false) {
+    auto bigTokenMemRes = self.alloc_.allocAlignedArray<Token>(capacity, ALLOC_CACHE_ALIGN);
+    if(bigTokenMemRes.hasValue() == false) {
         return std::variant<Tokenizer, CompileError>(CompileError::createOutOfMemory());
     }
-    Token* bigTokens = bigMemRes.value();
+    Token* bigTokens = bigTokenMemRes.value();
+    auto bigEndsMemRes = self.alloc_.allocAlignedArray<uint32_t>(capacity, ALLOC_CACHE_ALIGN);
+    if(bigTokenMemRes.hasValue() == false) {
+        self.alloc_.freeAlignedArray(bigTokens, capacity, ALLOC_CACHE_ALIGN);
+        return std::variant<Tokenizer, CompileError>(CompileError::createOutOfMemory());
+    }
+    uint32_t* bigEnds = bigEndsMemRes.value();
 
     uint32_t start = 0;
     uint32_t tokenIter = 0;
@@ -71,6 +80,7 @@ std::variant<Tokenizer, CompileError> Tokenizer::create(Allocator allocator, Str
         sy_assert(token.tag() != TokenType::Error, "Unexpected token error");
 
         bigTokens[tokenIter] = token;
+        bigEnds[tokenIter] = newStart;
         tokenIter += 1;
         start = newStart;
         
@@ -82,35 +92,72 @@ std::variant<Tokenizer, CompileError> Tokenizer::create(Allocator allocator, Str
     // Now shrink the allocation to not use insane amounts of memory
     // Doesn't need to be specially aligned as the tokens will be used for reading only,
     // so no real false sharing.
-    auto shrunkMemRes = self.alloc_.allocArray<Token>(tokenIter);
-    if(shrunkMemRes.hasValue() == false) {
-        self.alloc_.freeArray(bigTokens, capacity);
+    auto shrunkTokenMemRes = self.alloc_.allocArray<Token>(tokenIter);
+    if(shrunkTokenMemRes.hasValue() == false) {
+        self.alloc_.freeAlignedArray(bigTokens, capacity, ALLOC_CACHE_ALIGN);
+        self.alloc_.freeAlignedArray(bigEnds, capacity, ALLOC_CACHE_ALIGN);
         return std::variant<Tokenizer, CompileError>(CompileError::createOutOfMemory());
     }
-    Token* smallTokens = shrunkMemRes.value();
+    Token* smallTokens = shrunkTokenMemRes.value();
+    auto shrunkEndsMemRes = self.alloc_.allocAlignedArray<uint32_t>(capacity, ALLOC_CACHE_ALIGN);
+    if(shrunkEndsMemRes.hasValue() == false) {
+        self.alloc_.freeAlignedArray(bigTokens, capacity, ALLOC_CACHE_ALIGN);
+        self.alloc_.freeAlignedArray(bigEnds, capacity, ALLOC_CACHE_ALIGN);
+        self.alloc_.freeArray(smallTokens, capacity);
+        return std::variant<Tokenizer, CompileError>(CompileError::createOutOfMemory());
+    }
+    uint32_t* smallEnds = shrunkEndsMemRes.value();
+
     for(uint32_t i = 0; i < tokenIter; i++) {
         smallTokens[i] = bigTokens[i];
     }
+    for(uint32_t i = 0; i < tokenIter; i++) {
+        smallEnds[i] = bigEnds[i];
+    }
     self.tokens_ = smallTokens;
+    self.ends_ = smallEnds;
     self.len_ = tokenIter;
 
-    self.alloc_.freeArray(bigTokens, capacity);
+    self.alloc_.freeAlignedArray(bigTokens, capacity, ALLOC_CACHE_ALIGN);
+    self.alloc_.freeAlignedArray(bigEnds, capacity, ALLOC_CACHE_ALIGN);
 
     return std::variant<Tokenizer, CompileError>(std::move(self));
 }
 
-Token TokenIter::next()
+std::optional<Token> TokenIter::next()
 {
+    sy_assert(this->current_ != nullptr, "Invalid iterator");
+
     const Token* endToken = &this->tokenizer_->tokens_[this->tokenizer_->len_];
-    sy_assert((this->current_ + 1) != endToken, "Out of bounds token access");
+    if((this->current_ + 1) == endToken) {
+        this->current_ = nullptr;
+        return std::nullopt;
+    }
 
     this->current_ += 1;
+    return std::optional<Token>(*this->current_);
+}
+
+Token TokenIter::current() const
+{
+    sy_assert(this->current_ != nullptr, "Invalid iterator");
     return *this->current_;
 }
 
-Token TokenIter::peek() const
+std::optional<Token> TokenIter::peek() const
 {
+    sy_assert(this->current_ != nullptr, "Invalid iterator");
+
     const Token* endToken = &this->tokenizer_->tokens_[this->tokenizer_->len_];
-    sy_assert((this->current_ + 1) != endToken, "Out of bounds token access");
-    return this->current_[1];
+    if((this->current_ + 1) == endToken) {
+        return std::nullopt;
+    }
+    return std::optional<Token>(this->current_[1]);
+}
+
+uint32_t TokenIter::currentEnd() const
+{
+    sy_assert(this->current_ != nullptr, "Invalid iterator");
+    const ptrdiff_t diff = this->current_ - this->tokenizer_->tokens_;
+    return this->tokenizer_->ends_[diff];
 }
--- lib/src/compiler/tokenizer/tokenizer.hpp
+++ lib/src/compiler/tokenizer/tokenizer.hpp
@@ -7,6 +7,7 @@
 #include "../../mem/allocator.hpp"
 #include "../compile_info.hpp"
 #include <variant>
+#include <optional>
 
 class TokenIter;
 
@@ -29,23 +30,44 @@ private:
     friend class TokenIter;
 
     sy::Allocator alloc_;
+    sy::StringSlice source_;
+    /// Tokens found within the source, in order of appearance. When used with
+    /// `Tokenizer::ends_` member, also specifies the range. Uses struct of
+    /// arrays for better cache utilization, since most tokens don't need a
+    /// range.
     const Token* tokens_ = nullptr;
+    /// The end character index of the tokens found within the source, in order
+    /// of appearance. For many tokens this won't matter, but for literals
+    /// and identifiers, it's very useful to figure out how many characters
+    /// the token spans. Uses struct of arrays for better cache utilization,
+    /// since most tokens don't need a range.
+    const uint32_t* ends_ = nullptr;
     uint32_t len_ = 0;
 };
 
 class TokenIter {
 public:
 
-    Token next();
+    /// @brief Steps forward the iterator by one token. If there are no more
+    /// tokens, the iterator will invalidate itself and return `std::nullopt`.
+    /// @return The next token, or `std::nullopt` if there is no next token.
+    [[nodiscard]] std::optional<Token> next();
 
-    [[nodiscard]] Token current() const { return *current_; }
+    /// @return The token at the current iterator position.
+    [[nodiscard]] Token current() const;
 
-    [[nodiscard]] Token peek() const;
+    /// @return The token after the current one, or `std::nullopt` .
+    /// if there is no next token.
+    [[nodiscard]] std::optional<Token> peek() const;
+
+    /// @brief Some tokens, notably literals and identifiers, need to know how
+    /// many characters they span so that their data can be parsed out.
+    /// @return The end index (exclusive) of the token found in the source text.
+    [[nodiscard]] uint32_t currentEnd() const;
 
 private:
     const Token*        current_;
     const Tokenizer*    tokenizer_;
-
 };
 
 #endif //SY_COMPILER_TOKENIZER_TOKENIZER_HPP_
