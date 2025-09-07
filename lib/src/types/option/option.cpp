8a24c8b639c5f24eb88bd103939c20d9c30d262e
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2025-09-07 17:20:49 -0400

    option type with predictable memory layout

---
+++ lib/src/types/option/option.cpp
@@ -0,0 +1,139 @@
+#include "option.hpp"
+#include "../../util/assert.hpp"
+
+void sy::detail::debugAssertPtrNotNull(const void* ptr, const char* errMsg) { sy_assert(ptr != nullptr, errMsg); }
+
+void sy::detail::debugAssertOptionHasValue(bool hasVal, const char* errMsg) { sy_assert(hasVal, errMsg); }
+
+#if SYNC_LIB_WITH_TESTS
+
+#include "../../doctest.h"
+
+using sy::Option;
+
+TEST_CASE("Option ptr empty") {
+    Option<void*> opt;
+    CHECK_FALSE(opt.hasValue());
+    CHECK_FALSE(opt);
+    CHECK(!opt);
+}
+
+TEST_CASE("Option ptr not empty") {
+    void* somePtr = reinterpret_cast<void*>(1);
+    Option<void*> opt = somePtr;
+    CHECK(opt.hasValue());
+    CHECK(opt);
+    CHECK_FALSE(!opt);
+    CHECK_EQ(opt.value(), somePtr);
+}
+
+TEST_CASE("Option ptr empty non void") {
+    Option<int*> opt;
+    CHECK_FALSE(opt.hasValue());
+    CHECK_FALSE(opt);
+    CHECK(!opt);
+}
+
+TEST_CASE("Option ptr not empty non void") {
+    int val = 5;
+    Option<int*> opt = &val;
+    CHECK(opt.hasValue());
+    CHECK(opt);
+    CHECK_FALSE(!opt);
+    CHECK_EQ(*opt.value(), 5);
+}
+
+TEST_CASE("Option reference empty") {
+    Option<int&> opt;
+    CHECK_FALSE(opt.hasValue());
+    CHECK_FALSE(opt);
+    CHECK(!opt);
+}
+
+TEST_CASE("Option reference not empty") {
+    int val = 5;
+    Option<int&> opt = val;
+    CHECK(opt.hasValue());
+    CHECK(opt);
+    CHECK_FALSE(!opt);
+    CHECK_EQ(opt.value(), 5);
+    opt.value() = 6;
+    CHECK_EQ(opt.value(), 6);
+    CHECK_EQ(val, 6);
+}
+
+TEST_CASE("Option const reference not empty") {
+    int val = 5;
+    Option<const int&> opt = val;
+    CHECK(opt.hasValue());
+    CHECK(opt);
+    CHECK_FALSE(!opt);
+    CHECK_EQ(opt.value(), 5);
+}
+
+TEST_CASE("Option value empty") {
+    Option<int> opt;
+    CHECK_FALSE(opt.hasValue());
+    CHECK_FALSE(opt);
+    CHECK(!opt);
+}
+
+TEST_CASE("Option value not empty") {
+    int val = 5;
+    Option<int> opt = val;
+    CHECK(opt.hasValue());
+    CHECK(opt);
+    CHECK_FALSE(!opt);
+    CHECK_EQ(opt.value(), 5);
+    opt.value() = 6;
+    CHECK_EQ(opt.value(), 6);
+    CHECK_EQ(val, 5); // does not change initial value cause is copy
+}
+
+struct ComplexType {
+    int* ptr = nullptr;
+    inline static int aliveCount = 0;
+
+    ComplexType(int in) {
+        ptr = new int(in);
+        aliveCount += 1;
+    }
+
+    ~ComplexType() {
+        if(ptr == nullptr) return;
+        delete ptr;
+        ptr = nullptr;
+        aliveCount -= 1;
+    }
+
+    ComplexType(ComplexType&& other) {
+        ptr = other.ptr;
+        other.ptr = nullptr;
+    }
+};
+
+TEST_CASE("Option complex type empty") {
+    CHECK_EQ(ComplexType::aliveCount, 0);
+    Option<ComplexType> opt;
+    CHECK_EQ(ComplexType::aliveCount, 0); // didn't construct
+    CHECK_FALSE(opt.hasValue());
+    CHECK_FALSE(opt);
+    CHECK(!opt);
+}
+
+TEST_CASE("Option complex type empty") {
+    {
+        CHECK_EQ(ComplexType::aliveCount, 0);
+        Option<ComplexType> opt = ComplexType(5);
+        CHECK_EQ(ComplexType::aliveCount, 1); // did construct
+        CHECK(opt.hasValue());
+        CHECK(opt);
+        CHECK_FALSE(!opt);
+        CHECK_EQ(*opt.value().ptr, 5);
+        *opt.value().ptr = 6;
+        CHECK_EQ(*opt.value().ptr, 6);
+    } // destructor called
+    CHECK_EQ(ComplexType::aliveCount, 0);
+}
+
+#endif
---
+++ lib/src/types/option/option.hpp
@@ -0,0 +1,150 @@
+//! API
+#pragma once
+#ifndef SY_TYPES_OPTION_OPTION_HPP_
+#define SY_TYPES_OPTION_OPTION_HPP_
+
+#include "../../core.h"
+#include <new>
+#include <type_traits>
+
+namespace sy {
+namespace detail {
+void debugAssertPtrNotNull(const void* ptr, const char* errMsg);
+void debugAssertOptionHasValue(bool hasVal, const char* errMsg);
+} // namespace detail
+
+template <typename T, typename Enable = void> class SY_API Option final {};
+
+template <typename T> class SY_API Option<T, std::enable_if_t<std::is_pointer_v<T>>> final {
+  public:
+    Option() = default;
+    Option(T ptr) : ptr_(ptr) {}
+    [[nodiscard]] bool hasValue() const { return ptr_ != nullptr; }
+    [[nodiscard]] operator bool() const { return this->hasValue(); }
+    [[nodiscard]] bool operator!() const { return ptr_ == nullptr; }
+    [[nodiscard]] T value() {
+        detail::debugAssertPtrNotNull(ptr_, "Bad sy::Option access: Optional pointer is nullptr");
+        return ptr_;
+    }
+    [[nodiscard]] const T value() const {
+        detail::debugAssertPtrNotNull(ptr_, "Bad sy::Option access: Optional pointer is nullptr");
+        return ptr_;
+    }
+
+  private:
+    T ptr_ = nullptr;
+};
+
+template <typename T> class SY_API Option<T, std::enable_if_t<std::is_reference_v<T>>> final {
+  private:
+    using NonReference = std::remove_reference_t<T>;
+    using PtrT = std::conditional_t<std::is_const_v<T>, const NonReference*, NonReference*>;
+
+  public:
+    Option() = default;
+    Option(T ref) : ptr_(&ref) {}
+    [[nodiscard]] bool hasValue() const { return ptr_ != nullptr; }
+    [[nodiscard]] operator bool() const { return this->hasValue(); }
+    [[nodiscard]] bool operator!() const { return ptr_ == nullptr; }
+    [[nodiscard]] T value() {
+        detail::debugAssertPtrNotNull(ptr_, "Bad sy::Option access: Optional reference is empty");
+        return *ptr_;
+    }
+    [[nodiscard]] const T value() const {
+        detail::debugAssertPtrNotNull(ptr_, "Bad sy::Option access: Optional reference is empty");
+        return *ptr_;
+    }
+
+  private:
+    PtrT ptr_ = nullptr;
+};
+
+template <typename T> class SY_API Option<T, std::enable_if_t<!std::is_reference_v<T> && !std::is_pointer_v<T>>> final {
+  public:
+    Option() = default;
+    Option(T&& val) noexcept : hasVal_(true), val_(std::move(val)) {}
+    Option(const T& val) : hasVal_(true), val_(val) {}
+    ~Option() noexcept {
+        if (hasVal_) {
+            val_.val.~T();
+        }
+        hasVal_ = false;
+    }
+    Option(Option&& other) noexcept : hasVal_(other.hasVal_) {
+        if (hasVal_) {
+            new (&val_.val) T(std::move(other.val_.val));
+            other.hasVal_ = false;
+        }
+    }
+    Option& operator=(Option&& other) noexcept {
+        if (this == &other)
+            return *this;
+
+        if (hasVal_) {
+            val_.val.~T();
+        }
+        hasVal_ = other.hasVal_;
+        if (hasVal_) {
+            new (&val_.val) T(std::move(other.val_.val));
+            other.hasVal_ = false;
+        }
+        return *this;
+    }
+    Option(const Option& other) : hasVal_(other.hasVal_) {
+        if (hasVal_) {
+            new (&val_.val) T(other.val_.val);
+        }
+    }
+    Option& operator=(const Option& other) noexcept {
+        if (this == &other)
+            return *this;
+
+        if (hasVal_) {
+            val_.val.~T();
+        }
+        hasVal_ = other.hasVal_;
+        if (hasVal_) {
+            new (&val_.val) T(other.val_.val);
+        }
+        return *this;
+    }
+    [[nodiscard]] bool hasValue() const { return hasVal_; }
+    [[nodiscard]] operator bool() const { return this->hasValue(); }
+    [[nodiscard]] bool operator!() const { return !hasVal_; }
+    [[nodiscard]] T& value() {
+        detail::debugAssertOptionHasValue(hasVal_, "Bad sy::Option access: Optional value is empty");
+        return val_.val;
+    }
+    [[nodiscard]] const T& value() const {
+        detail::debugAssertOptionHasValue(hasVal_, "Bad sy::Option access: Optional value is empty");
+        return val_.val;
+    }
+    [[nodiscard]] T&& take() {
+        detail::debugAssertOptionHasValue(hasVal_, "Bad sy::Option access: Optional value is empty");
+        this->hasVal_ = false;
+        return std::move(val_.val);
+    }
+
+  private:
+#if _WIN32 // allow non dll-interface types to be used here
+#pragma warning(push)
+#pragma warning(disable : 4251)
+#endif
+    union SY_API Val {
+        uint8_t empty;
+        T val;
+        Val() : empty(0) {}
+        Val(T&& inVal) noexcept : val(std::move(inVal)) {}
+        Val(const T& inVal) noexcept : val(inVal) {}
+        ~Val() noexcept {}
+    };
+#if _WIN32
+#pragma warning(pop)
+#endif
+
+    bool hasVal_ = false;
+    Val val_;
+};
+} // namespace sy
+
+#endif // SY_TYPES_OPTION_OPTION_HPP_
