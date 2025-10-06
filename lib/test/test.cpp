9d5a8909357ca50f2d658e224ff088df50065ffa
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2025-10-06 18:50:23 -0400

    basic cmake

--- lib/test/test.cpp
+++ lib/test/test.cpp
@@ -1,6 +1,4 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "../src/doctest.h"
-#include "../src/testing/child_process.hpp"
-#include <cstring>
 
 int main(int argc, char** argv) { return doctest::Context(argc, argv).run(); }
