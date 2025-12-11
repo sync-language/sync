4700aa9f02baad18d509f7df879320ab89293870
gabkhanfig <gabkhanfig@gmail.com>
2025-12-10 22:19:30 -0500

    test suite on mac

--- test/run_tests.py
+++ test/run_tests.py
@@ -5,7 +5,6 @@ from typing import List
 import platform
 import time
 
-
 def run_test_sequentially(test_name: str, test_command: str):
     print(f"Running: {test_name}...")
     start_time = time.perf_counter()
@@ -103,6 +102,26 @@ if OPERATING_SYSTEM == "Windows" and IS_X64:
     #     "Windows arm64",
     #     'cmake -G "Visual Studio 17 2022" -B build-arm64 -A ARM64 -DCMAKE_BUILD_TYPE=Debug -DSYNC_LIB_DOCTEST_ADD_CTESTS=OFF -DSYNC_LIB_ASAN=OFF && cmake --build build-arm64 --config Debug --parallel && ctest --test-dir build-arm64 -C Debug --output-on-failure'
     # )
+
+if OPERATING_SYSTEM == "Darwin" and IS_ARM64:
+    print("MacOS arm64 detected. Running the following test suite:")
+    print(" - MacOS arm64")
+    print(" - MacOS x64 with Rosetta")
+    print(" - Ubuntu x64 with docker")
+    print(" - Ubuntu arm64 with docker")
+    print(" - Ubuntu riscv64 with docker")
+    print(" - WASM with Ubuntu docker + emscripten")
+
+    run_test_sequentially(
+        "MacOS arm64",
+        'cmake -B build-x64 -DCMAKE_OSX_ARCHITECTURES="arm64" -DCMAKE_BUILD_TYPE=Debug -DSYNC_LIB_DOCTEST_ADD_CTESTS=OFF -DSYNC_LIB_ASAN=OFF && cmake --build build-x64 --config Debug --parallel && ctest --test-dir build-x64 -C Debug --output-on-failure'
+    )
+
+    run_test_sequentially(
+        "MacOS x64",
+        'cmake -B build-x64 -DCMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_BUILD_TYPE=Debug -DSYNC_LIB_DOCTEST_ADD_CTESTS=OFF -DSYNC_LIB_ASAN=OFF && cmake --build build-x64 --config Debug --parallel && ctest --test-dir build-x64 -C Debug --output-on-failure'
+    )
+
 
 run_test_sequentially(
     "Docker Ubuntu x64",
