1d7c28fc391971550709d8b3962e75ca303f1965
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2026-01-19 21:20:17 -0500

    no =1

--- lib/test/test_runner.cpp
+++ lib/test/test_runner.cpp
@@ -1,9 +1,13 @@
 #if SYNC_LIB_WITH_TESTS
 
-#define DOCTEST_CONFIG_NO_POSIX_SIGNALS
+// #define DOCTEST_CONFIG_NO_POSIX_SIGNALS redefine issue
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "test_runner.h"
 #include "../src/doctest.h"
+
+#if defined(DOCTEST_CONFIG_POSIX_SIGNALS)
+#error "DOCTEST_CONFIG_POSIX_SIGNALS should not be defined when DOCTEST_CONFIG_NO_POSIX_SIGNALS is set"
+#endif
 
 extern "C" {
 SY_API int run_sync_tests(int argc, char** argv) { return doctest::Context(argc, argv).run(); }
